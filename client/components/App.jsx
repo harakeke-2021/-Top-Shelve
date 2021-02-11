d9405f281075f290dcf4a9dc9d86c615662d82df
Christo Karadjov <clkaradjov@gmail.com>
2021-02-12 12:40:50 +1300

    bassic css layout done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <Route path={'/css'} component={CssSetup}/>
-      <CookieList />
+      {/* <CookieList /> */}
     </>
   )
 }
--- client/index.js
+++ client/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 
 // REDUX
 import { createStore } from 'redux'
@@ -14,7 +13,6 @@ const store = createStore(reducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 document.addEventListener('DOMContentLoaded', () => {
-
   render(
     <Provider store={store}>
       <Router>
