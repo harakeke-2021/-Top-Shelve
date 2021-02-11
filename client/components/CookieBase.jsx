c8d9529616c8c7bbcef73165a10224ce6768267a
Peter <peter.lim91@gmail.com>
2021-02-12 11:33:25 +1300

    state records selcted cookie base

--- client/components/CookieBase.jsx
+++ client/components/CookieBase.jsx
@@ -2,11 +2,17 @@ import React, { useState, useEffect } from 'react'
 
 function CookieBase() {
   const cookies = ['plain', 'shortbread', 'peanutbutter', 'chocolate']
+  const [selectedBase, setSelectedBase] = useState('')
+
   return (
     <div className='cookie-base'>
       {cookies.map((cookie) => {
         return (
-          <img key={cookie} className='cookie' src={`/${cookie}.png`}></img>
+          <img
+            onClick={() => setSelectedBase(cookie)}
+            key={cookie}
+            className='cookie'
+            src={`/${cookie}.png`}></img>
         )
       })}
     </div>
